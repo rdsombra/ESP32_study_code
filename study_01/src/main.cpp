7214fae270a5d3ea58c3d9fb84979c2681a7c428
Rafael Sombra <rafaelsombra1@gmail.com>
2021-06-14 18:23:38 -0300

    STUDY01: fixing typos

--- study_01/src/main.cpp
+++ study_01/src/main.cpp
@@ -5,8 +5,8 @@
 /*pin mapping*/ 
 #define LED 2
 
-TaskHandle_t taks1Handle = NULL;
-TaskHandle_t taks2Handle = NULL;
+TaskHandle_t task1Handle = NULL;
+TaskHandle_t task2Handle = NULL;
 
 /*Task prototypes*/
 void vTask1(void *pvParameters);
@@ -14,8 +14,8 @@ void vTask2(void *pvParameters);
 
 void setup() {
   Serial.begin(9600);
-  xTaskCreate(vTask1,"TASK1",configMINIMAL_STACK_SIZE,NULL,1,&taks1Handle);
-  xTaskCreate(vTask2,"TASK2",configMINIMAL_STACK_SIZE+1024,NULL,2,&taks1Handle);
+  xTaskCreate(vTask1,"TASK1",configMINIMAL_STACK_SIZE,NULL,1,&task1Handle);
+  xTaskCreate(vTask2,"TASK2",configMINIMAL_STACK_SIZE+1024,NULL,2,&task2Handle);
 }
 
 void loop() {
