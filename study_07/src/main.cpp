562ddd81c897171ddd7b0ace9dcfbbec7829409c
Rafael Sombra <rafaelsombra1@gmail.com>
2021-06-24 18:47:38 -0300

    STUDY07 Studying how to work with queues and pass values between tasks using them

---
+++ study_07/src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freeRTOS/task.h"
+#include "freertos/queue.h"
+
+#define LED 2
+
+QueueHandle_t xQ;
+TaskHandle_t xTask1Handler;
+TaskHandle_t xTask2Handler;
+
+void vTask1(void *pvParameters);
+void vTask2(void *pvParameters);
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  BaseType_t xReturned;
+  xQ = xQueueCreate(5, sizeof(int));
+
+  if (NULL == xQ){
+    Serial.println("It was not possible to create the queue");
+    /*blocking program exec*/
+    while(1);
+  }
+
+  xReturned = xTaskCreate(
+    vTask1, "Task1", configMINIMAL_STACK_SIZE+1024, NULL, 1, &xTask1Handler);
+  if(pdFAIL == xReturned){
+    Serial.println("Failed to create task1.");
+    /*blocking program exec*/
+    while (1);    
+  }
+  xReturned = xTaskCreate(
+    vTask2, "Task2", configMINIMAL_STACK_SIZE+1024, NULL, 1, &xTask2Handler);
+  if(pdFAIL == xReturned){
+    Serial.println("Failed to create task2.");
+    /*blocking program exec*/
+    while (1);    
+  }
+}
+
+void loop() {
+  digitalWrite(LED, !digitalRead(LED));
+  vTaskDelay(pdMS_TO_TICKS(1000));
+}
+
+void vTask1(void *pvParameters){
+  int count = 0;
+  while(1){
+    if(count < 10){
+      xQueueSend(xQ, &count, portMAX_DELAY);
+      count++;
+    }
+    else{
+      count = 0;
+      vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+void vTask2(void *pvParameters){
+  int received_value = 0;
+  while(1){
+    if(pdTRUE == xQueueReceive(xQ, &received_value, pdMS_TO_TICKS(1000))){
+      Serial.println("Received value: " + String(received_value));
+    }
+    else Serial.println("TIMEOUT!!!");
+  }
+}
