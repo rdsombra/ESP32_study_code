7269b605d549ad3e9f04fa1311b33b78fae474d5
Rafael Sombra <rsombra@MacBook-Air-de-Rafael.local>
2023-12-29 10:12:57 -0300

    study_03 fixing variable names and adding comments.

--- study_03/src/main.cpp
+++ study_03/src/main.cpp
@@ -9,8 +9,8 @@
 /*pin mapping*/ 
 #define LED 2
 
-TaskHandle_t task1Handle = NULL;
-TaskHandle_t task2Handle = NULL;
+TaskHandle_t xTask1Handle = NULL;
+TaskHandle_t xTask2Handle = NULL;
 
 /*Task prototypes*/
 void vTask1(void *pvParameters);
@@ -18,8 +18,8 @@ void vTask2(void *pvParameters);
 
 void setup() {
   Serial.begin(9600);
-  xTaskCreate(vTask1,"TASK1",configMINIMAL_STACK_SIZE,NULL,1,&task1Handle);
-  xTaskCreate(vTask2,"TASK2",configMINIMAL_STACK_SIZE+1024,NULL,2,&task2Handle);
+  xTaskCreate(vTask1,"TASK1",configMINIMAL_STACK_SIZE,NULL,1,&xTask1Handle);
+  xTaskCreate(vTask2,"TASK2",configMINIMAL_STACK_SIZE+1024,NULL,2,&xTask2Handle);
 }
 
 void loop() {
@@ -42,14 +42,16 @@ void vTask2(void *pvParameters){
   int count = 0;
   while (1){
     Serial.println("Task2: " + String(count++));
+    /* suspend task1 when count==10 */
     if(count == 10){
       Serial.println("suspending task1...");
-      vTaskSuspend(task1Handle);
+      vTaskSuspend(xTask1Handle);
       digitalWrite(LED, LOW);
     }
+    /* resume task1 when count==15 */
     if(count == 15){
       Serial.println("calling back task 1...");
-      vTaskResume(task1Handle);
+      vTaskResume(xTask1Handle);
       count = 0;
       Serial.println("back to beginning");
     }
