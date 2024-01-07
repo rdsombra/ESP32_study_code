ec77c3c661301117c06a862606db9372ed0c88dd
Rafael Sombra <rsombra@MacBook-Air-de-Rafael.local>
2024-01-07 17:39:57 -0300

    Adding a timer to turn-off the external LED

--- quick_demonstration/src/main.cpp
+++ quick_demonstration/src/main.cpp
@@ -1,14 +1,14 @@
 /* This project intends to bring a quick demonstration of some 
 * FreeRTOS features, such as task creation, reading ADC values from
-* an input, memory management, and response to ISRs.
+* an input, timers, queues, and response to ISRs.
 * code by: Rafael Sombra.
 */
-
 
 #include <Arduino.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/timers.h"
 
 /* pin mapping.*/
 #define LED_A 2
@@ -18,11 +18,13 @@
 
 TaskHandle_t xTask1Handle = NULL;
 QueueHandle_t xQueue = NULL;
+TimerHandle_t xTimer1 = NULL;
 TaskHandle_t xTaskMonitorHandle = NULL;
 
 //function declarations:
 void vTreatButtonEvent();
 void vTaskMonitor(void *vpParameters);
+void vTimer1Callback(TimerHandle_t xTimer1);
 
 void setup() {
   Serial.begin(9600);
@@ -30,13 +32,15 @@ void setup() {
   pinMode(LED_B, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
   BaseType_t xReturned;
-  xQueue = xQueueCreate(5, sizeof(int));
+  xQueue = xQueueCreate(10, sizeof(int));
   
   if (NULL == xQueue)
   {
     Serial.println("It was not possible to create the queue! Blocking exec!!!");
     while(1);
   }
+
+  xTimer1 = xTimerCreate("TIMER1", pdMS_TO_TICKS(2000), pdFALSE, 0, vTimer1Callback);
   xReturned = xTaskCreatePinnedToCore(vTaskMonitor, "TaskMonitor", 
     configMINIMAL_STACK_SIZE +1024, NULL, 2, &xTaskMonitorHandle, APP_CPU_NUM);
   if (pdFAIL == xReturned) 
@@ -61,31 +65,32 @@ void loop() {
 void vTreatButtonEvent() {
   int xSensorReading = 0;
   BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+
   xSensorReading = analogRead(ANALOG_INPUT);
   if (errQUEUE_FULL == xQueueSendFromISR(xQueue, &xSensorReading, &xHigherPriorityTaskWoken))
   {
     Serial.println("The queue is full right now!!!");
   }
-  else Serial.println("Value sent to the Q: " +String(xSensorReading));
-  // mudar estado do LED_B
-  // timer para desligar o LED_B
-}
-
-/* Reads the analogic input from the pin passed by parameter,
-* and prints the value to the Serial. Values will be showed on terminal. */
-void vReadAnalogInput(int xInputPin){
-  Serial.println("ADC Value: " +String(analogRead(xInputPin)));
+  else {
+    Serial.println("Value sent to the Q: " +String(xSensorReading));
+    digitalWrite(LED_B, HIGH);
+    xTimerStart(xTimer1, 0);
+  }
 }
 
 void vTaskMonitor(void *vpParameters){
   int xReceived;
+
   while (1)
   {
-    if (pdTRUE == xQueueReceive(xQueue, &xReceived, pdMS_TO_TICKS(1000)))
+    if (pdTRUE == xQueueReceive(xQueue, &xReceived, pdMS_TO_TICKS(500)))
     {
       Serial.println("Received value: " +String(xReceived));
       vTaskDelay(pdMS_TO_TICKS(500));
     }
   }
-  
+}
+
+void vTimer1Callback(TimerHandle_t xTimer){
+  digitalWrite(LED_B, LOW);
 }
